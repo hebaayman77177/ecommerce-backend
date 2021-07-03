03461f7d8bcdcc5bf4782044311c0f8501d5deb1
heba <hebaayman77177>
2021-07-03 11:04:30 +0200

    update order apis

--- api/order/models/order.js
+++ api/order/models/order.js
@@ -1,8 +1,23 @@
-'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    // Called before an entry is created
+    async beforeCreate(data) {
+      console.log("🚀 ~ file: order.js ~ line 5 ~ beforeCreate ~ data", data);
+      const orderItems = [];
+      for (let i = 0; i < data.order_items.length; i += 1) {
+        let entity = await strapi.services["order-item"].create(
+          data.order_items[i]
+        );
+        console.log(
+          "🚀 ~ file: order.js ~ line 8 ~ data.orderItems.map ~ entity",
+          entity
+        );
+        orderItems.push(entity.id);
+      }
+      data.order_items = orderItems;
+      console.log("🚀 ~ file: order.js ~ line 15 ~ beforeCreate ~ data", data);
+    },
+    // Called after an entry is created
+    afterCreate(result) {},
+  },
+};
